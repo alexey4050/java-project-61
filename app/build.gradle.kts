dfb2af6708c36d502a912350c49ae5e9f6545141
Alexey Kiselev <alexey4050@mail.ru>
2024-10-30 17:43:52 +0300

    Добавлен Makefail, создан класс Cli, задан стандартный пток ввода

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,4 +23,8 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
 
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        System.out.println("Welcome to th Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("may I have your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello," + name + "!");
+    }
+}
