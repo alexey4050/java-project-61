57070839181b4b9d8f373e3f1fdda95dd366b5a1
Alexey Kiselev <alexey4050@mail.ru>
2024-11-04 14:01:40 +0300

    Correct class App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,53 +9,42 @@ import hexlet.code.games.Prime;
 public class App {
     public static void main(String[] args) {
 
-        while (true) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = Cli.SCANNER.nextInt();
+        Cli.SCANNER.nextLine();
 
-            boolean validInput = false;
-            int choice = 0;
-            while (!validInput) {
-                System.out.print("Your choice: ");
-                try {
-                    choice = Cli.SCANNER.nextInt();
-                    Cli.SCANNER.nextLine();
-                    validInput = true;
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input. Please enter a valid number.");
-                }
-            }
-            switch (choice) {
-                case 1:
-                    Cli.greet();
-                    break;
-                case 2:
-                    Engine.run(new Even());
-                    break;
-                case 3:
-                    Engine.run(new Calc());
-                    break;
-                case 4:
-                    Engine.run(new GCD());
-                    break;
-                case 5:
-                    Engine.run(new Progression());
-                    break;
-                case 6:
-                    Engine.run(new Prime());
-                    break;
-                case 0:
-                    System.out.println("Goodbye!");
-                    return;
-                default:
-                    System.out.println("Invalid choice. Please try again. " + choice);
-            }
+        switch (choice) {
+            case 1:
+                Cli.greet();
+                break;
+            case 2:
+                Engine.run(new Even());
+                break;
+            case 3:
+                Engine.run(new Calc());
+                break;
+            case 4:
+                Engine.run(new GCD());
+                break;
+            case 5:
+                Engine.run(new Progression());
+                break;
+            case 6:
+                Engine.run(new Prime());
+                break;
+            case 0:
+                System.out.println("Goodbye!");
+                return;
+            default:
+                System.out.println("Invalid choice. Please try again. " + choice);
         }
     }
 }
