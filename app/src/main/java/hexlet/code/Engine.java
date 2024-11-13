2e1a629645459f60eba53b4898eb04b75fc77fa4
Alexey Kiselev <alexey4050@mail.ru>
2024-11-13 08:28:11 +0300

    Change class Engine and Utils

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,168 +1,40 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Prime;
-
 import java.util.Scanner;
 
 public class Engine {
 
-    private static final int ROUNDS_COUNTS = 3;
-    private static final Scanner SCANNER = new Scanner(System.in);
-    private static final int MAX_VALUE = 100;
+    public static final int ROUNDS_COUNTS = 3;
 
-    public static void run(String gameName) {
+    public static void run(String rules, String[][] gameRounds) {
+        var scanner = new Scanner(System.in);
+        String userName = greetMessage(scanner);
+
+        System.out.println(rules);
+
+        for (var round : gameRounds) {
+            System.out.println("Question: " + round[0]);
+            System.out.println("Your answer: ");
+            String userAnswer = scanner.next();
+
+            if (userAnswer.toLowerCase().equals(round[1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
+                System.out.println("Correct answer was '" + round[1] + "'. ");
+                System.out.println("Let's try again, " + userName + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName + "!");
+    }
+
+    static String greetMessage(Scanner scanner) {
         System.out.println();
         System.out.println("Welcome to the Brain Games!");
-        String userName = Cli.getUserName();
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-
-        switch (gameName) {
-            case "Even":
-                playEvenGame(userName);
-                break;
-            case "Calc":
-                playCalcGame(userName);
-                break;
-            case "GCD":
-                playGCDGame(userName);
-                break;
-            case "Progression":
-                playProgressionGame(userName);
-                break;
-            case "Prime":
-                playPrimeGame(userName);
-                break;
-            default:
-                System.out.println("Game not found");
-        }
-    }
-
-    private static void playEvenGame(String userName) {
-        System.out.println(Even.getDescription());
-        int rounds = 0;
-        while (rounds < ROUNDS_COUNTS) {
-            int number = Utils.getRandomInt(0, MAX_VALUE);
-            System.out.println("Question: " + number);
-            String userAnswer = getUserAnswer();
-
-            if (Even.isCorrectAnswer(userAnswer, number)) {
-                System.out.println("Correct!");
-                rounds++;
-            } else {
-                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
-                System.out.println("Correct answer was '" + Even.getCorrectAnswer(number) + "'. ");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (rounds == ROUNDS_COUNTS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
-    }
-
-    private static void playCalcGame(String userName) {
-        System.out.println(Calc.getDescription());
-        int rounds = 0;
-        while (rounds < ROUNDS_COUNTS) {
-            int numberOne = Utils.getRandomInt(0, MAX_VALUE);
-            int numberTwo = Utils.getRandomInt(0, MAX_VALUE);
-            String currentQuestion = Calc.generateQuestion(numberOne, numberTwo);
-            System.out.println("Question: " + currentQuestion);
-            String userAnswer = getUserAnswer();
-
-            if (Calc.isCorrectAnswer(userAnswer, numberOne, numberTwo, currentQuestion)) {
-                System.out.println("Correct!");
-                rounds++;
-            } else {
-                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
-                System.out.print("Correct answer was '");
-                char operation = currentQuestion.trim().split(" ")[1].charAt(0);
-                System.out.println(Calc.getCorrectAnswer(numberOne, numberTwo, operation) + "'. ");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (rounds == ROUNDS_COUNTS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
-    }
-
-    private static void playGCDGame(String userName) {
-        System.out.println(GCD.getDescription());
-        int rounds = 0;
-        while (rounds < ROUNDS_COUNTS) {
-            int numberOne = Utils.getRandomInt(0, MAX_VALUE);
-            int numberTwo = Utils.getRandomInt(0, MAX_VALUE);
-            System.out.println("Question: " + numberOne + " " + numberTwo);
-            String userAnswer = getUserAnswer();
-
-            if (GCD.isCorrectAnswer(userAnswer, numberOne, numberTwo)) {
-                System.out.println("Correct!");
-                rounds++;
-            } else {
-                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
-                System.out.println("Correct answer was '" + GCD.getCorrectAnswer(numberOne, numberTwo) + "'. ");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (rounds == ROUNDS_COUNTS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
-    }
-
-    private static void playProgressionGame(String userName) {
-        System.out.println(Progression.getDescription());
-        int rounds = 0;
-        while (rounds < ROUNDS_COUNTS) {
-            String question = Progression.generateQuestion();
-            System.out.println("Question: " + question);
-            String userAnswer = getUserAnswer();
-
-            if (Progression.isCorrectAnswer(userAnswer, question)) {
-                System.out.println("Correct!");
-                rounds++;
-            } else {
-                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
-                System.out.println("Correct answer was '" + Progression.getCorrectAnswer(question) + "'. ");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (rounds == ROUNDS_COUNTS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
-    }
-
-    private static void playPrimeGame(String userName) {
-        System.out.println(Prime.getDescription());
-        int rounds = 0;
-        while (rounds < ROUNDS_COUNTS) {
-            int number = Utils.getRandomInt(0, MAX_VALUE);
-            System.out.println("Question: " + number);
-            String userAnswer = getUserAnswer();
-
-            if (Prime.isCorrectAnswer(userAnswer, number)) {
-                System.out.println("Correct!");
-                rounds++;
-            } else {
-                System.out.print("'" + userAnswer + "' is wrong " + "answer ;(. ");
-                System.out.println("Correct answer was '" + Prime.getCorrectAnswer(number) + "'. ");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (rounds == ROUNDS_COUNTS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
-    }
-
-    private static String getUserAnswer() {
-        System.out.print("Your answer: ");
-        return SCANNER.nextLine();
+        return userName;
     }
 }
--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,9 +3,17 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
+
+    static final int MIN_RANDOM_NUMBER = 0;
+    static final int MAX_RANDOM_NUMBER = 100;
+
     private static final Random RANDOM = new Random();
 
-    public static int getRandomInt(int min, int max) {
-        return RANDOM.nextInt(max - min + 1) + min;
+    public static int generateRandomNumber(int lowerRangeLimit, int upperRangeLimit) {
+        return lowerRangeLimit + RANDOM.nextInt(upperRangeLimit - lowerRangeLimit);
+    }
+
+    public static int generateRandomNumber() {
+        return generateRandomNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 }
