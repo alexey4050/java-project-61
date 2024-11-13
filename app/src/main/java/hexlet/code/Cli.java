c06bd1ac8d46b9bea36714b6bd52cdbb9ea75020
Alexey Kiselev <alexey4050@mail.ru>
2024-11-13 08:26:09 +0300

    Change class Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,19 +2,14 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class Cli {
+public final class Cli {
     public static final Scanner SCANNER = new Scanner(System.in);
 
     public static void greet() {
-        System.out.println();
+
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = SCANNER.nextLine();
         System.out.println("Hello, " + name + "!");
-    }
-
-    public static String getUserName() {
-        System.out.print("May I have your name? ");
-        return SCANNER.nextLine();
     }
 }
